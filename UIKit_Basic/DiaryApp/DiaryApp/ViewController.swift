064a8878bf3f5dec87d8de9fd9f72abe22c1c28c
the-hye <ganzi410@gmail.com>
2025-03-19 15:16:00 +0900

    Init DiaryApp Project

---
+++ UIKit_Basic/DiaryApp/DiaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiaryApp
+//
+//  Created by Jihye Seok on 3/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
